39d0adbc446b80d0ca82bfbbc15a81404b09690c
LAPTOP-LFE5JF3B\HP <rdwimauliddani@gmail.com>
2023-05-11 20:28:56 +0700

    data

--- data.py
+++ data.py
@@ -1,27 +1,45 @@
 import streamlit as st
-tab1,tab2 = st.tabs(["Mengisi Form Data Karir Mahasiswa","Data Karir Mahasiswa"])
-with tab1 :
-    st.title("Data Karir Mahasiswa")
-    nama=st.text_input("Nama")
-    nim=st.text_input("NIM")
-    prodi=st.text_input("Prodi")
-    lulusan=st.text_input("Tahun Lulusan")
-    bekerja=st.text_input("Bekerja")
-    send=st.button("Kirim")
-    if send :
-        st.success("Terkirim")
-    if send:
-        with tab2 :
-            yang_terkirim1 = nama
-            yang_terkirim2 = nim
-            yang_terkirim3 = prodi
-            yang_terkirim4 = lulusan
-            yang_terkirim5 = bekerja
-            st.text("===============================================================================")
-            (f"Nama    : {yang_terkirim1}")
-            (f"NIM     : {yang_terkirim2}")
-            (f"Prodi   : {yang_terkirim3}")
-            (f"Lulusan : {yang_terkirim4}")
-            (f"Bekerja : {yang_terkirim5}")
-            st.text("===============================================================================")
 
+st.title("Data Karir Mahasiswa")
+
+# Initialize an empty list to store the data entries
+data_entries = []
+
+while True:
+    nama = st.text_input("Nama")
+    nim = st.text_input("NIM")
+    prodi = st.text_input("Prodi")
+    lulusan = st.text_input("Tahun Lulusan")
+    bekerja = st.text_input("Bekerja")
+
+    # Create a dictionary to store the data for this entry
+    data_entry = {
+        "Nama": nama,
+        "NIM": nim,
+        "Prodi": prodi,
+        "Tahun Lulusan": lulusan,
+        "Bekerja": bekerja
+    }
+
+
+    # Add the data entry to the list of entries if the user clicks the "Kirim" button
+    if st.button("Kirim"):
+        data_entries.append(data_entry)
+        st.success("Terkirim")
+        
+     # Ask the user if they want to enter another data entry
+    if not st.button("Tambah Data Lagi"):
+        break
+        
+   
+           
+        
+        
+# Display the table with all of the data entries
+if data_entries:
+    st.write("Data Karir Mahasiswa:")
+    st.table(data_entries)   
+else:
+    st.write("Belum ada data yang dimasukkan.")
+    
+    
